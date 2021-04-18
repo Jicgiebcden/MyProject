1be494a5efc348631a9258093db22a14932cb21b
Jicgiebcden <hevqiang86@163.com>
2021-04-18 16:54:51 +0800

    在初始化mongo时使用复制集，并设置读取偏好和写顾虑。

--- springmongo/src/main/java/com/huangym/config/MongoConfig.java
+++ springmongo/src/main/java/com/huangym/config/MongoConfig.java
@@ -1,5 +1,8 @@
 package com.huangym.config;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,6 +13,9 @@ import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
 
 import com.mongodb.Mongo;
 import com.mongodb.MongoOptions;
+import com.mongodb.ReadPreference;
+import com.mongodb.ServerAddress;
+import com.mongodb.WriteConcern;
 
 /**
  * Mongo配置
@@ -52,9 +58,32 @@ public class MongoConfig extends AbstractMongoConfiguration {
 	public Mongo mongo() throws Exception {
 		logger.info("make mongo (" + host + ":" + port + ")");
 		Mongo m = new Mongo(host, port);
+		
+		/** 使用多个服务器复制集初始化mongo
+		List<ServerAddress> seeds = new ArrayList<ServerAddress>();
+		ServerAddress address = new ServerAddress(host, port);
+		seeds.add(address);
+		Mongo mongo = new Mongo(seeds);
+		*/
+		
 		MongoOptions options = m.getMongoOptions();
 		options.autoConnectRetry = true;
 		options.socketKeepAlive = true;
+		
+		/**
+		// 设置读取偏好，选择从哪个复制集成员读取数据。
+		options.readPreference = ReadPreference.primary();	// 只从主服务器读取数据，这是默认的读取偏好。如果用户显示指定使用标签读取偏好，那么该读取偏好将被阻塞。
+		options.readPreference = ReadPreference.primaryPreferred();	// 读取将被重定向至主服务器，如果没有可用的主服务器，那么读取将被重定向至某个辅助服务器。
+		options.readPreference = ReadPreference.secondary();	// 读取将被重定向至辅助服务器节点。如果没有可用的辅助服务器，该选项将会生成异常。
+		options.readPreference = ReadPreference.secondaryPreferred();	// 该读取将被重定向至辅助服务器；如果没有可用的辅助服务器，那么读取将被重定向至主服务器。该选项对应旧的slaveOk方法。
+		options.readPreference = ReadPreference.nearest();	// 从最近的节点读取数据，不论它是主服务器还是辅助服务器。该选项通过网络延迟决定使用哪个节点。
+		
+		// 设置写顾虑。
+		options.w = 1;
+		options.writeConcern = WriteConcern.SAFE;
+		options.wtimeout = 0;
+		*/
+		
 		return m;
 	}
 
